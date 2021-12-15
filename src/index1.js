2e30d4516627e39c75ae035597a5152b98e93882
ozgenkacar <55004814+ozgenkacar@users.noreply.github.com>
2021-12-14 19:50:26 -0500

    Create index1.js

---
+++ src/index1.js
@@ -0,0 +1,24 @@
+const dropdown = document.getElementById("languageOption");
+dropdown.onchange = function() {
+    let codeInput = document.getElementById("codeInput");
+    codeInput.value = "";
+
+}
+
+
+const getBigOBtn = document.getElementById("getBigOBtn");
+getBigOBtn.onclick = function() {
+    const codeInput = document.getElementById("codeInput");
+    const code = codeInput.value.trim();
+    const result = parseInput(code);
+
+    const resultShow = document.getElementById("resultShow");
+    resultShow.innerHTML = result;
+}
+
+function parseInput(code) {
+    let newlineSplit = code.split("\n")
+    let forStatements = getForStatements(newlineSplit);
+    let result = getBigONotation(forStatements);
+    return result;
+}
